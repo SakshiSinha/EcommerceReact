10b153bc5666b410e171c89330c75b35d4e99e81
SakshiSinha <sakshisinha202326@gmail.com>
2020-03-11 16:22:39 +0530

    react

---
+++ build/precache-manifest.a3642ef21e04136c8c2e53b3d44731fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10263ed1e6599250ebdea0c5747ca8c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10d18e48ffdf3b361008",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "632e3e77b3b1c1605a8d",
+    "url": "/static/js/2.3dbb50e0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.3dbb50e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10d18e48ffdf3b361008",
+    "url": "/static/js/main.90122f4c.chunk.js"
+  },
+  {
+    "revision": "8f5a7289ea8ac69f63d1",
+    "url": "/static/js/runtime-main.6a1dc1d1.js"
+  }
+]);
